f530004f77689c1b75708600ed806094a9fbfac8
Erik Waters <erik.waters@teampalmtree.com>
2014-03-12 20:36:54 -0700

    Remove test comment

--- GDMRadio/src/com/example/gdmradio/MainActivity.java
+++ GDMRadio/src/com/example/gdmradio/MainActivity.java
@@ -11,8 +11,6 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-	
-	// Test comment
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
